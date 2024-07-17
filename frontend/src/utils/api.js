f00f471e8a2f5a54f18578648056342c38b538c5
Siddharth Khatri <siddxharth@gmail.com>
2024-07-17 23:01:26 +0530

    Fix docker

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_APP_BACKEND_URL;
-const apiPort = import.meta.env.VITE_APP_BACKEND_PORT;
-
 const api = axios.create({
-    baseURL: `http://${apiUrl}:${apiPort}/api/v1`,
+    baseURL: `http://localhost:3001/api/v1`,
     headers: {
         "Content-Type": "application/json",
     },
