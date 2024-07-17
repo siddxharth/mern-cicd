d8bdf63604a6ce84a5c36913ff02683ba48771ab
Siddharth Khatri <siddxharth@gmail.com>
2024-07-17 11:26:00 +0530

    Add env vars

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@ const apiUrl = import.meta.env.VITE_APP_BACKEND_URL;
 const apiPort = import.meta.env.VITE_APP_BACKEND_PORT;
 
 const api = axios.create({
-    baseURL: `http://${"localhost"}:${"3001"}/api/v1`,
+    baseURL: `http://${apiUrl}:${apiPort}/api/v1`,
     headers: {
         "Content-Type": "application/json",
     },
